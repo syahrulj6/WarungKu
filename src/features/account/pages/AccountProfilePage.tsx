bd7222f73152fea737846e41079690cd892e1aba
syahrul jay <syahruljaylani73@gmail.com>
2025-04-21 14:58:18 +0700

    feat: update profile

--- src/features/account/pages/AccountProfilePage.tsx
+++ src/features/account/pages/AccountProfilePage.tsx
@@ -17,6 +17,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form } from "~/components/ui/form";
 import { ProfileSettingFormInner } from "../components/ProfileSettingFormInner";
 import { Button } from "~/components/ui/button";
+import { toast } from "sonner";
+import { TRPCClientError } from "@trpc/client";
+import { Loader } from "lucide-react";
+import { Skeleton } from "~/components/ui/skeleton";
 
 const AccountProfilePage = () => {
   const { data: getProfileData, isLoading } = api.profile.getProfile.useQuery();
@@ -36,6 +40,26 @@ const AccountProfilePage = () => {
     }
   }, [getProfileData, form]);
 
+  const updateProfile = api.profile.updateProfile.useMutation({
+    onSuccess: ({ username }) => {
+      form.reset({ username });
+      toast.success("Berhasil update profile");
+    },
+    onError: (err) => {
+      if (err instanceof TRPCClientError && err.message === "USERNAME_USED") {
+        form.setError("username", { message: "Username sudah digunakan" });
+      }
+      toast.error("Gagal update profile");
+    },
+  });
+
+  const handleUpdateProfileSubmit = (values: ProfileSettingFormSchema) => {
+    updateProfile.mutate({
+      username:
+        values.username !== getProfileData?.username ? values.username : "",
+    });
+  };
+
   return (
     <DashboardLayout>
       <div className="space-y-4 p-4 md:p-6">
@@ -52,7 +76,14 @@ const AccountProfilePage = () => {
             <CardHeader className="-my-2">Informasi Profil</CardHeader>
             <Separator />
             <CardContent>
-              {!isLoading && getProfileData && (
+              {isLoading ? (
+                <div className="space-y-4">
+                  <div className="space-y-2">
+                    <Skeleton className="h-4 w-24" />
+                    <Skeleton className="h-10 w-full" />
+                  </div>
+                </div>
+              ) : getProfileData ? (
                 <Form {...form}>
                   <ProfileSettingFormInner
                     defaultValues={{
@@ -60,20 +91,28 @@ const AccountProfilePage = () => {
                     }}
                   />
                 </Form>
-              )}
+              ) : null}
             </CardContent>
             <Separator />
             <CardFooter className="-my-2 flex justify-end gap-2">
               <Button
                 size="sm"
                 variant="outline"
-                disabled={!form.formState.isDirty}
+                disabled={!form.formState.isDirty || updateProfile.isPending}
                 onClick={() => form.reset()}
               >
                 Batal
               </Button>
-              <Button size="sm" disabled={!form.formState.isDirty}>
-                Simpan
+              <Button
+                size="sm"
+                disabled={!form.formState.isDirty || updateProfile.isPending}
+                onClick={form.handleSubmit(handleUpdateProfileSubmit)}
+              >
+                {updateProfile.isPending ? (
+                  <Loader className="animate-spin" />
+                ) : (
+                  "Simpan"
+                )}
               </Button>
             </CardFooter>
           </Card>
--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
 
 export const profileRouter = createTRPCRouter({
   getProfile: privateProcedure.query(async ({ ctx }) => {
@@ -18,4 +19,52 @@ export const profileRouter = createTRPCRouter({
 
     return profile;
   }),
+
+  updateProfile: privateProcedure
+    .input(
+      z.object({
+        username: z
+          .string()
+          .min(3)
+          .max(16)
+          .regex(
+            /^[a-zA-Z0-9_]+$/,
+            "Username can only contain letters, numbers, and underscores",
+          )
+          .transform((val) => val.toLowerCase().trim()),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { db, user } = ctx;
+      const { username } = input;
+
+      if (username) {
+        const usernameExists = await db.user.findFirst({
+          where: {
+            username,
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (usernameExists) {
+          throw new TRPCError({
+            code: "CONFLICT",
+            message: "Username is already taken",
+          });
+        }
+      }
+
+      const updatedUser = await db.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          username,
+        },
+      });
+
+      return updatedUser;
+    }),
 });
