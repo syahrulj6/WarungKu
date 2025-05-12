bbe40246975d83430319bd173f95e01c01c6e78f
syahrul jay <syahruljaylani73@gmail.com>
2025-05-12 18:17:24 +0700

    feat: initial history page

---
+++ src/features/warung/pages/HistoryPage.tsx
@@ -0,0 +1,11 @@
+import { WarungDashboardLayout } from "~/components/layout/WarungDashboardLayout";
+
+const HistoryPage = () => {
+  return (
+    <WarungDashboardLayout withRightPanel={true}>
+      <div className="">History Page</div>
+    </WarungDashboardLayout>
+  );
+};
+
+export default HistoryPage;
