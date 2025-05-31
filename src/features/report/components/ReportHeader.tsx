2a28ac101d8d30a4abd02c4c700e41ae41051780
syahrul jay <syahruljaylani73@gmail.com>
2025-05-31 17:44:41 +0700

    ui: report header

--- src/features/report/components/ReportHeader.tsx
+++ src/features/report/components/ReportHeader.tsx
@@ -1,15 +1,37 @@
 import { CalendarIcon, ChevronDown, NotebookIcon } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 
 export const ReportHeader = () => {
   return (
     <div className="flex w-full flex-col gap-2 md:flex-row md:items-center md:gap-4">
-      <Button size="sm" variant="outline" className="text-muted-foreground">
-        <CalendarIcon /> Last 7 Days <ChevronDown />
-      </Button>
-      <Button size="sm" variant="outline" className="text-muted-foreground">
-        <NotebookIcon /> Export <ChevronDown />
-      </Button>
+      <Select>
+        <SelectTrigger className="w-fit">
+          <CalendarIcon />
+          <SelectValue placeholder="Last 7 Days" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="7-days">Last 7 Days</SelectItem>
+          <SelectItem value="30-days">Last 30 Days</SelectItem>
+          <SelectItem value="1-year">Lasy 1 Year</SelectItem>
+        </SelectContent>
+      </Select>
+      <Select>
+        <SelectTrigger className="w-fit">
+          <NotebookIcon />
+          <SelectValue placeholder="Export" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="pdf">Pdf</SelectItem>
+          <SelectItem value="excel">Excel</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   );
 };
